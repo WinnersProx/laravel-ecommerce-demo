fd1c452aa734a3fa7392967f481b73d103169f27
Winner <bihames4vainqueur@gmail.com>
2022-01-15 13:54:09 +0200

    feat(auth): add the authentication scaffolding

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', 'HomeController@index');

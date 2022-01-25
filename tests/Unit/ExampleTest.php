46c58525584e79d95a0031aabafa2c60f397033d
Winner <bihames4vainqueur@gmail.com>
2022-01-25 11:47:06 +0200

    testing: unit - add basic unit test feature

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+
+    public function test_project_is_up()
+    {
+        $this->assertTrue(true);
+    }
+}
